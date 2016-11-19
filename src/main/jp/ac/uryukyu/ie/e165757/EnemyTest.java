5e3a86a61024bb1808e555017a761af25f78ca1b
Shota Nagamine <e165757@ie.u-ryukyu.ac.jp>
2016-11-19 17:35:20 +0900

    report4

---
+++ src/main/jp/ac/uryukyu/ie/e165757/EnemyTest.java
@@ -0,0 +1,14 @@
+package main.jp.ac.uryukyu.ie.e165757;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165757 on 2016/11/19.
+ */
+public class EnemyTest {
+    @org.junit.Test
+    public void attack() throws Exception {
+
+    }
+
+}
