dd00b6ed96c0ab0a6b97663bf48930dc467b0d22
Shota Nagamine <e165757@ie.u-ryukyu.ac.jp>
2016-11-24 13:51:23 +0900

    report4

--- src/main/jp/ac/uryukyu/ie/e165757/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165757/Enemy.java
@@ -40,9 +40,11 @@ public class Enemy {  String name;
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(dead = false) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
